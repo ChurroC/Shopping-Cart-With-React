a2cd0236dd4ae5c9a166b65fa98310c8af523fd0
ChurroC <charanvenkatchandran@gmail.com>
2022-11-14 21:51:45 -0600

    x
    c

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,6 @@ import App from '@/App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter } from 'react-router-dom'
 
-/**
- * @file src/index.tsx
- * @description The entry point for the application.
- */
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <BrowserRouter>
